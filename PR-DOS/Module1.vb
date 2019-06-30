95460944e3ba346982470d90e47ce19eb9c079f8
Luky1234567890 <41448721+Luky1234567890@users.noreply.github.com>
2019-06-30 22:38:07 +0200

    1.0 intial release upload srccode
    like.. github for vs sucks right? :D

---
+++ PR-DOS/Module1.vb
@@ -0,0 +1,45 @@
+﻿Module Module1
+
+    Sub Main()
+restart:
+        Console.Clear()
+        Console.WriteLine("PR-DOS")
+        Console.WriteLine("----------------------")
+        Console.WriteLine("The command prompt by Nelinka. Ver. 1.0")
+        Console.WriteLine("TIP: For list of all commands, type list!")
+        Console.WriteLine("-----------------------------------------------")
+beginning:
+        Console.Write("PR-DOS>")
+        Dim theinput = Console.ReadLine()
+        If theinput = "close" Then
+            Console.WriteLine("close>Ready to close PR-DOS! Press ENTER to close PR-DOS.")
+            Console.ReadLine()
+            End
+        ElseIf theinput = "clear" Then
+            Console.Clear()
+            GoTo beginning
+        ElseIf theinput = "list" Then
+            Console.WriteLine("list>close = Closes PR-DOS.")
+            Console.WriteLine("list>clear = Clears the console.")
+            Console.WriteLine("list>list = Lists all avaiable commands. (Currently in use.)")
+            Console.WriteLine("list>restart = Restarts the console.")
+            Console.WriteLine("list>about = About PR-DOS and Nelinka.")
+            GoTo beginning
+        ElseIf theinput = "restart" Then
+            GoTo restart
+            End
+        ElseIf theinput = "about" Then
+            Console.WriteLine("about>PR-DOS made by Nelinka.")
+            Console.WriteLine("about>Ver. 1.0 -- CHANGELOG")
+            Console.WriteLine("about>-----------------------------------")
+            Console.WriteLine("about>Intial release. PRRD XD")
+            GoTo beginning
+        Else
+            Console.WriteLine("PR-DOS>This command does not exist!")
+            GoTo beginning
+        End If
+
+
+    End Sub
+
+End Module
---
+++ PR-DOS/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Obecné informace o sestavení se řídí přes následující 
+' sadu atributů. Změnou hodnot těchto atributů se upraví informace
+' přidružené k sestavení.
+
+' Zkontrolujte hodnoty atributů sestavení.
+
+<Assembly: AssemblyTitle("PR-DOS")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PR-DOS")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+<Assembly: Guid("ffc3361a-a53a-49d5-9b7a-1f24f2f3e428")>
+
+' Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+'
+'      Hlavní verze
+'      Podverze
+'      Číslo sestavení
+'      Revize
+'
+' Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize pomocí *, jak je vidět níže:
+' pomocí zástupného znaku * takto:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
